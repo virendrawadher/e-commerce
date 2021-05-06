de0df51a3cde1b7a9b99a067a30c85d9bb6d8157
virendrawadher <wadher32@gmail.com>
2021-05-06 21:22:22 +0530

    register  page

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 import {useState} from "react"
-import { Link, useNavigate } from "react-router-dom"
-import { useAuth } from "./authcontextprovider"
-import { Home } from "./home"
+import styles from "../css/style.module.css"
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "../context/authcontextprovider"
+import { useToast } from "../context/toastContext"
 
 const Register = () => {
     const [name, setName] = useState("")
@@ -10,6 +11,7 @@ const Register = () => {
     const [password, setPassword] = useState("")
     const [message, setMessage] = useState(null)
     const {setIsSignUp} = useAuth()
+    const { toast } = useToast()
     const navigate = useNavigate()
 
     const signUpHandler = async() => {
@@ -22,20 +24,40 @@ const Register = () => {
             setIsSignUp(true)
             setMessage(data.message)
             navigate("/login")
+            toast(`${data.message}`, {
+                type: "success",
+                position: "bottom-right",
+                autoClose: 3000,
+                hideProgressBar: true
+            })
         }else{
             setIsSignUp(false)
             setMessage(data.message)
+            toast(`${data.message}`, {
+                type: "success",
+                position: "bottom-right",
+                autoClose: 3000,
+                hideProgressBar: true
+            })
         }
 
     }
 
     return (
-        <div>
-            <input type = "text" value = {name} onChange = {(e) => setName(e.target.value)} placeholder = "Enter the name"/>
-            <input type = "email" value = {email} onChange = {(e) => setEmail(e.target.value)} placeholder = "Enter the email"/>
-            <input type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} placeholder = "Enter the password"/>
-            <button onClick = {signUpHandler}>Sign Up</button>
-            <div>{message}</div>
+        <div className = {styles.signinmodal}>
+            <div className = {styles.signincontent}>
+                <h2>Sign Up</h2>
+                <div className = {styles.signindata}>
+                    <label style = {{textAlign: "left", marginBottom: "0.2rem"}}>Enter user name</label>
+                    <input type = "text" value = {name} onChange = {(e) => setName(e.target.value)} style = {{padding: "0.5rem 1rem", borderRadius: "0.2rem", fontSize: "1.2rem", marginBottom: "1rem"}}/>
+                    <label style = {{textAlign: "left", marginBottom: "0.2rem"}}>Enter user name</label>
+                    <input type = "email" value = {email} onChange = {(e) => setEmail(e.target.value)} style = {{padding: "0.5rem 1rem", borderRadius: "0.2rem", fontSize: "1.2rem", marginBottom: "1rem"}}/>
+                    <label style = {{textAlign: "left", marginBottom: "0.2rem"}}>Enter user name</label>
+                    <input type = "password" value = {password} onChange = {(e) => setPassword(e.target.value)} style = {{padding: "0.5rem 1rem", borderRadius: "0.2rem", fontSize: "1.2rem", marginBottom: "1rem"}}/>
+                </div>   
+                <button onClick = {signUpHandler} className = {styles.cartbutton}>Sign Up</button>
+            </div>
+            {/* <div>{message}</div> */}
             {/* {
                 setTimeout(() => {
                     return <div>{message}</div>
